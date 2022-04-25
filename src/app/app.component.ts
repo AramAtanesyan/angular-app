8f3d0ee3b0e658168af77c09a1334f82b4089a16
Aram <a.g.atanesyan@gmail.com>
2022-04-25 19:01:06 +0400

    video 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: yellow;
+    }
+  `],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit{
   name = 'Aram';
