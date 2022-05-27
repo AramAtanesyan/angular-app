264b4aa66b9e2b9ba3206e3c0238d3bb6c6fa86a
Gago <gago@gmail.com>
2022-05-27 19:07:03 +0400

    27.05.22_yerevan_office

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import {RecipeService} from "../recipe.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,7 +12,7 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private activeRoute: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private activeRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.activeRoute.params.subscribe((params: Params) => {
@@ -27,6 +27,11 @@ export class RecipeDetailComponent implements OnInit {
 
   addToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.activeRoute})
   }
 
 
